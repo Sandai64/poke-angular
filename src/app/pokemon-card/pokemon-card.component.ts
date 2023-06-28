7bf296281d7ce0c72b20f64fe434b321d1a0f2b7
Erwan Egasse <contact@erwan.sh>
2023-06-28 11:21:58 +0200

    Draft initial pokemon-card component

---
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import Pokemon from '../interfaces/pokemon';
+
+@Component({
+  selector: 'app-pokemon-card',
+  templateUrl: './pokemon-card.component.html',
+  styleUrls: ['./pokemon-card.component.css']
+})
+export class PokemonCardComponent {
+  @Input() pokemon: Pokemon = {
+    name: '',
+    index: undefined,
+    localizedName: undefined,
+    sprite: undefined,
+    abilities: undefined,
+    moves: undefined,
+    type: undefined,
+    weight: undefined
+  };
+}
